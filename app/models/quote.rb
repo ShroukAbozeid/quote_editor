eb6e693493040df15a02087e233dc80a2394a3de
ShroukAbozeid <29339875+ShroukAbozeid@users.noreply.github.com>
2026-05-22 19:44:16 +0300

    empty state page when no quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -6,4 +6,17 @@ class Quote < ApplicationRecord
   scope :recent_first, -> { order(created_at: :desc) }
 
   broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, inserts_by: :prepend
+
+  after_create_commit :remove_empty_state
+  after_destroy_commit :add_empty_state, if: -> { company.quotes.empty? }
+
+  private
+
+  def add_empty_state
+    broadcast_append_to [ company, "quotes" ], target: "new_quote", partial: "quotes/empty_state"
+  end
+
+  def remove_empty_state
+    broadcast_remove_to [ company, "quotes" ], target: "empty_state"
+  end
 end
